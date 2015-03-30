eea7528858bb2aa7a3bb793ff055750e83aba563
Vinit Jain <vinit.jain@myntra.com>
2015-03-30 23:18:59 +0530

    SOme css changes

---
+++ js/content.js
@@ -0,0 +1,42 @@
+var WEB_DATA = {
+	"home" : "<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis cursus tortor. Nunc consectetuer diam ac odio." +
+			"Pellentesque vel mauris sit amet urna malesuada ornare. Curabitur venenatis est eget arcu. Donec vestibulum. Proin rutrum. " +
+			"Morbi commodo fringilla orci. Ut vel tortor. In ut velit. Vivamus hendrerit aliquam quam. Curabitur placerat eros vitae libero." +
+			"Pellentesque vitae velit at dui semper sodales. Curabitur accumsan ornare libero. Quisque auctor. Lorem ipsum dolor sit amet, " +
+			"consectetuer adipiscing elit. Nulla adipiscing porta ante Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis cursus " +
+			"tortor. Nunc consectetuer diam ac odio. Pellentesque vel mauris sit amet urna malesuada ornare. Curabitur venenatis est eget arcu. " +
+			"Donec vestibulum. Proin rutrum. Morbi commodo fringilla orci. Ut vel tortor. In ut velit. Vivamus hendrerit aliquam quam. Curabitur " +
+			"placerat eros vitae libero.Pellentesque vitae velit at dui semper sodales. Curabitur accumsan ornare libero. Quisque auctor. " +
+			" Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla adipiscing porta ante Lorem ipsum dolor sit amet, consectetuer " +
+			"adipiscing elit. Duis cursus tortor. Nunc consectetuer diam ac odio. Pellentesque vel mauris sit amet urna malesuada ornare. Curabitur " +
+			" venenatis est eget arcu. Donec vestibulum. Proin rutrum. Morbi commodo fringilla orci. Ut vel tortor. In ut velit. Vivamus hendrerit " +
+			"aliquam quam. Curabitur placerat eros vitae libero.Pellentesque vitae velit at dui semper sodales. Curabitur accumsan ornare libero. " +
+			" Quisque auctor. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla adipiscing porta ante </p>",
+	"about": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis cursus tortor. Nunc consectetuer diam ac odio. " +
+				"Pellentesque vel mauris sit amet urna malesuada ornare. Curabitur venenatis est eget arcu. Donec vestibulum. Proin rutrum. " +
+				"Morbi commodo fringilla orci. Ut vel tortor. In ut velit. Vivamus hendrerit aliquam quam. Curabitur placerat eros vitae " +
+				"libero.Pellentesque vitae velit at dui semper sodales. Curabitur accumsan ornare libero. Quisque auctor. Lorem ipsum dolor sit " +
+				"amet, consectetuer adipiscing elit. Nulla adipiscing porta ante",
+	"service": '<p> ' +
+				'</p>',
+	"contact" : '<form method="post" action="mailto:vinitjain16@gmail.com" ' +
+					' name="ContactForm"> ' +
+    				' <div class="text-field"> ' +
+    					'<div class="text"><label class="label" for="name-id">Name: </label></div>' +
+    					'<div class="input"><input type="text" size="40" name="Name" placeholder="Your Name" id="name-id"></div> ' +
+    				'</div>' +
+    				' <div class="text-field"> ' +
+    					'<div class="text"><label class="label" for="email-id">E-mail Address: </label></div>' +
+    					'<div class="input"><input type="text" size="40" name="Email" placeholder="your email@email.com" id="email-id"></div> ' +
+    				'</div>' +
+    				' <div class="text-field"> ' +
+    					'<div class="text"><label class="label" for="subject-id">Subject: </label></div>' +
+    					'<div class="input"><input type="text" size="40" name="Subject" placeholder="Your Name" id="subject-id"></div> ' +
+    				'</div>' +
+    				' <div class="text-field"> ' +
+    					'<div class="text"><label class="label" for="comment-id">Comments: </label></div>' +
+    					'<div class="input"><textarea cols="55" name="Comment" id="comment-id">  </textarea></div> ' +
+    				'</div>' +
+    				' <div class="text-field"><input type="submit" value="Submit" name="submit"> ' +
+    				' <input type="reset" value="Reset" name="reset"></div> </form>'
+};
---
+++ js/main.js
@@ -0,0 +1,73 @@
+(function () {
+
+	var contentNode = document.getElementById("ppl-main-content"),
+		ValidateContactForm,
+		contactListener = false,
+		navNode = document.getElementById("navigation"),
+		homeData,
+		validateEmail =  function (val) {
+          // Regular Expression copied from stack overflow [46155]
+	        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	        return re.test(val);
+	    };
+
+	navNode.addEventListener('click', function (e) {
+		var target = e.target || e.srcElement, type, data;
+		if (target) {
+			if (target.nodeName !== 'A') return;
+			e.stopPropagation();
+			e.preventDefault();
+			type = target.getAttribute('data-attr');
+			data = WEB_DATA[type];
+			contentNode.innerHTML = data;
+			if (!contactListener) {
+				ValidateContactForm(contentNode);
+				contactListener = true;
+			}
+		}
+	}, false);
+
+	/* Initial Load */
+	homeData = WEB_DATA.home;
+	contentNode.innerHTML = homeData;
+
+	ValidateContactForm = function (node) {
+		var form = node.getElementsByTagName('form');
+		form[0].addEventListener("submit", function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+			var name = document.ContactForm.Name,
+				email = document.ContactForm.Email,
+				subject = document.ContactForm.Subject,
+				comment = document.ContactForm.Comment, check = true;
+
+			if (name.value.trim() === "")
+			{
+				name.classList.add("error-border");
+				check = false;
+			}
+			if (comment.value.trim() === "")
+			{
+				comment.classList.add("error-border");
+				check = false;
+			}
+			if (subject.value.trim() === "")
+			{
+				subject.classList.add("error-border");
+				check = false;
+			}
+
+			if (email.value.trim() === "")
+			{
+				email.classList.add("error-border");
+				check = false;
+			} else {
+				check = validateEmail(email.value);
+				if (!check) {
+					email.classList.add("error-border");
+				}
+			}
+	    	return check;
+	    });
+	}
+})();
